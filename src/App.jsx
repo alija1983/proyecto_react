7e55d42225266644d57179d13cd14b5f1a3b798a
Nicolás Alija <nicolasalija@gmail.com>
2022-07-19 23:09:28 -0300

    navbar solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import './App.css';
-import navbar from "./components/navbar/Navbar"
+import './App.css'
+import Navbar from './components/navbar/Navbar'
 
-
-
-const App = () => {
+function App() {
   return (
     <div>
-        
-<Navbar></Navbar>
+<Navbar/>
 
     </div>
   )
-}
+  }
 
 export default App
---
+++ src/App1.jsx
@@ -0,0 +1,17 @@
+
+import './App.css';
+import navbar from './components/navbar/Navbar'
+
+
+
+const App = () => {
+  return (
+    <div>
+        
+<Navbar/>
+
+    </div>
+  )
+}
+
+export default App
