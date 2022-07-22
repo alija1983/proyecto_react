4013b905049f9b82a6d2620ece52688930d3e29d
Nicolás Alija <nicolasalija@gmail.com>
2022-07-21 21:03:39 -0300

    desafio landing 01

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import CartWidget from './components/navbar/CartWidget'
 import Navbar from './components/navbar/Navbar'
 
 function App() {
   return (
     <div>
 <Navbar/>
-
+<CartWidget/>
     </div>
   )
   }
---
+++ src/components/navbar/CartWidget.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const CartWidget = () => {
+  return (
+    <div>
+
+    <form class="form-inline my-2 my-lg-0">
+   
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit"> <ion-icon name="cart-outline"></ion-icon> Carrito</button>
+    </form>
+
+    </div>
+  )
+}
+
+export default CartWidget
